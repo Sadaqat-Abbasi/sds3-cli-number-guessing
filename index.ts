b0d170874682b9d729e59524cb8e85d49d2daf4b
Sadaqat-Abbasi <sadaqataliabbasi110@gmail.com>
2024-04-19 17:08:40 +0500

    cli base game

---
+++ index.ts
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessNumber",
+    type: "number",
+    message: "please guess a number between 1 to 6",
+  },
+]);
+if (answer.userGuessNumber === randomNumber) {
+  console.log("congratulations! you guessed correct number");
+} else {
+  console.log("you guessed wrong number");
+}
